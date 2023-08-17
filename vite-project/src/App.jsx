ede219ebc5c979b856141d3db0238832249c7c64
Sajarappuz <sajarmohammed71@gmail.com>
2023-08-17 19:59:46 +0530

    second

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -3,13 +3,30 @@ import React from 'react'
 import "./index.css"
 import Menu from './components/Menu'
 import Navbar from './components/Navbar'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './pages/Home';
+import Video from './pages/Video';
 
 const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home/>,
+    },
+    {
+      path: "/video",
+      element: <Video/>,
+    },
+  ]);
   return (
     <div className='flex'>
       <Menu/>
       <div className='flex-[7]'>
       <Navbar/>
+      <RouterProvider router={router} />
       </div>
     </div>
   )
--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import "../index.css"
+import SearchIcon from '@mui/icons-material/Search';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='sticky top-0 h-[60px] '>
+          <div className='flex items-center justify-end h-[100%] relative'>
+              <div className='w-[40%] absolute left-0 right-0 mx-auto flex items-center justify-between border-solid border-[1px] border-slate-700 rounded-md'>
+                 <input type="text"  className='bg-transparent border-none w-[90%]'/>
+                 <SearchIcon/>
+                </div> 
+          <button className='font-bold rounded-md border-solid border-2 border-blue-300 max-w-max px-3 py-1'>SignIn</button>
+             </div> 
+
+    </div>
   )
 }
 
